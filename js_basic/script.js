1f3f996197d374ed2f07c0f54822c0817d90d369
Ankit Kumar Singh <Ankitrajput5641@gmail.com>
2026-02-10 17:43:06 +0530

    01_folder done

---
+++ js_basic/script.js
@@ -0,0 +1,12 @@
+// var h1 = document.createElement("h1");
+
+// h1.innerHTML = "Hello World";
+// document.body.appendChild(h1);
+// console.log(h1);
+
+//  there are two types of export in js ie named export and default export..
+// in named export we can export multiple things and we have to import them with the same name as we exported them. 
+// but in default export we can export only one thing and we can import it with any name we want.
+
+// import arr from "./app.js";
+// console.log(arr);
