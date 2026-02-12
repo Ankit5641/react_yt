2ba3a1f8a49aa6e53caa307b83a376395d6539ab
Ankit Kumar Singh <Ankitrajput5641@gmail.com>
2026-02-12 15:46:48 +0530

    Initial commit

---
+++ 02_components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <h2>lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate!</h2>
+    </div>
+  )
+}
+
+export default Card
---
+++ 02_components/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+        <h2>ankit singh </h2>
+     </div>
+  )
+}
+
+export default Navbar
